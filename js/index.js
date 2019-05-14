00e3eefa43760b69d0dcad6725e2dd1d4a75d972
York Cook <yorkcook@Johnny-Castle.lan1>
2019-05-14 17:42:28 -0400

    WIP: 3 down

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 // Your code goes here
+
+const letsGo  = document.querySelector(".text-content");
+
+letsGo.addEventListener("mouseover", function (event){   
+
+    event.target.style.color = "purple";
+  
+   
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+
+//   keydown
+
+
+const destination = document.querySelector(".intro");
+
+destination.addEventListener ("wheel", function(event) {  
+       
+    document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80)" 
+    
+    setTimeout(function(){
+        event.body.style.backgroundImage = "";
+    }, 10);
+    }, false);
+
+    window.addEventListener('resize', () =>{
+        alert('Did it work?!')
+    })
+
+
+
+    // const destination = document.querySelector(".content-destination")
+    
+    // destination.addEventListener('dblclick', function () {
+    //     destination.toggle('large');
+    //   });
